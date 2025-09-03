e4d88f4149c6049b9be17606954495b8157a7ee5
nomena <hei.nomenjanahary@gmail.com>
2025-09-03 09:57:07 +0300

    l examen final

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app=FastAPI()
 
 @app.get("/ping")
 def pong():
  return{"message":"pong"}
+
+@app.get("/health")
+def health_root():
+ return{"message":"ok"}
+
+
+class phone(BaseModel):
+ identifiant: str
+ brand: str
+ model: str
+ characters:List[object]=[]
+
+resultat:List[phone]=[]
+
+@app.post("/phone")
+ def phone(phone:phone):
+
